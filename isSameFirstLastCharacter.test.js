b4fa38960d6f18fb3112c1d6747bfc28a6c26fe0
Abay <ibrayev.abay@gmail.com>
2023-10-04 20:35:41 -0600

    complete

--- isSameFirstLastCharacter.test.js
+++ isSameFirstLastCharacter.test.js
@@ -12,17 +12,27 @@ test("it should return true when the first and last character are the same", () 
 /* A test case for a string that does NOT have the same first and last 
    character and is 4+ characters long
 */
-test("", () => {});
+test("it should return false when the first and last character are NOT the same and is 4+ characters long", () => {
+   expect(isSameFirstLastCharacter("baaaess")).toBe(false)
+});
 
 // Edge Cases
 // A test case for a string with only 2 characters that are different
-test("", () => {});
+test("it should return false when string is 2 characters long and characters are different.", () => {
+   expect(isSameFirstLastCharacter("ab")).toBe(false)
+});
 
 // A test case for a string with only 2 characters that are the same
-test("", () => {});
+test("it should return true when string is 2 characters long and characters are same", () => {
+   expect(isSameFirstLastCharacter('aa')).toBe(true)
+});
 
 // A test case for a string with only 1 character
-test("", () => {});
+test("it should return true when string is one character", () => {
+   expect(isSameFirstLastCharacter('a')).toBe(true)
+});
 
 // A test case for an empty string
-test("", () => {});
+test("it should return true when string is empty", () => {
+   expect(isSameFirstLastCharacter('')).toBe(true)
+});
